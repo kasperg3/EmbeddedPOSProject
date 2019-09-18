1f76b4d9a5ded51354644b45a65782350b1b864e
oldblackeye <kasperg3@rocketmail.com>
2019-09-18 20:26:20 +0200

    initial commit

---
+++ exportToZybo.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# First argument has to be target location of the ssh target
+# the following arguments is the files that need to be transfered
+
+
+for var in "$@"
+do
+  if [ "$var" = "--help" ]
+   then
+    echo 'ARG1 has to be target location of the ssh target'
+    echo 'the following arguments is the files that need to be transfered. This can be unlimited.'
+    exit
+  fi
+done
+
+shift
+files=""
+
+for var in "$@"
+do
+  files="$files $var"
+done
+
+echo "Files to copy: $files"
+
+scp $files root@192.168.0.2:$1
---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+int main() {
+    int sum = 0;
+    int x;
+    std::string line;
+    std::ifstream inFile;
+    std::string fileName = "/dev/swipe";
+    inFile.open(fileName);
+    if (!inFile) {
+        std::cout << "Unable to open file: " << fileName;
+        exit(1); // terminate with error
+    }else{
+        std::cout << "Succesfully opened file: " << fileName;
+    }
+    while(true){
+        while (getline(inFile, line)) {
+            if(line.length() != 16){
+                for(int i = 0; i < line.length(); i++){
+                    if(i % 2 == 1){
+                        if(((int)line[i]-48) * 2 > 9){ sum += ((int)line[i]-48) * 2 - 9;}
+                    }else{
+                        sum += ((int)line[i]-48);
+                    }
+                }
+                if(sum%10){
+                    std::cout << "Correct checksum" << std::endl;
+                }else{
+                    std::cout << "Incorrect checksum" << std::endl;
+                }
+            }else{
+                std::cout << "Incorrect amount of digits in cardnumber" << std::endl;
+            }
+        }
+    }
+
+
+    inFile.close();
+    std::cout << "Sum = " << sum << std::endl;
+    return 0;
+}
