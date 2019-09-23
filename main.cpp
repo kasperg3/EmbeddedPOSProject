b1ab76ebf8052e1180b7d0006245984f3d2e0302
oldblackeye <kasperg3@rocketmail.com>
2019-09-23 08:56:43 +0200

    Reformat code

--- main.cpp
+++ main.cpp
@@ -10,8 +10,7 @@
 #include "include/CardReader.h"
 
 
-enum States
-{
+enum States {
     ST_IDLE,
     ST_FAILURE,
     ST_WAITFORSWIPE,
@@ -23,50 +22,53 @@ enum States
 States currentState = ST_IDLE;
 
 int main() {
-
     //Switch Case: MOVE THIS TO ANOTHER CLASS WHEN EXPANDING
-    while(currentState != ST_STOP){
+    while (currentState != ST_STOP) {
         switch (currentState) {
-            case ST_IDLE:{
+            case ST_IDLE: {
                 currentState = ST_WAITFORSWIPE;
                 break;
             }
-            case ST_FAILURE:{
+            case ST_FAILURE: {
                 std::cout << "Failed to validate card" << std::endl;
                 currentState = ST_IDLE;
                 break;
             }
-            case ST_WAITFORSWIPE:{
+            case ST_WAITFORSWIPE: {
                 std::cout << "Please swipe card" << std::endl;
                 CardReader cardReader;
                 cardReader.waitForSwipe();
-                if( !cardReader.luhnAlgorithm() ){ currentState = ST_FAILURE; break;}
+                if (!cardReader.luhnAlgorithm()) {
+                    currentState = ST_FAILURE;
+                    break;
+                }
                 currentState = ST_INPUTPIN;
                 break;
             }
-            case ST_INPUTPIN:{
+            case ST_INPUTPIN: {
                 std::string pinCode;
-                std::cout << "Please enter pin: "; std::cin >> pinCode;
-                if(pinCode.length() == 4)
+                std::cout << "Please enter pin: ";
+                std::cin >> pinCode;
+                if (pinCode.length() == 4)
                     currentState = ST_APPROVEAMOUNT;
                 else
                     std::cout << "Invalid pin. ";
                 break;
             }
-            case ST_APPROVEAMOUNT:{
+            case ST_APPROVEAMOUNT: {
                 std::cout << "Order total = 49.99 DKK, press 'a' to approve: ";
                 char input;
                 std::cin >> input;
-                if(input == 'a'){
+                if (input == 'a') {
                     std::cout << "Transaction approved,thank you for the purchase." << std::endl;
                     currentState = ST_STOP;
-                }else{
+                } else {
                     std::cout << "Transaction terminated, input recieved: " << input << std::endl;
                     currentState = ST_IDLE;
                 }
-            break;
+                break;
             }
-            case ST_STOP:{
+            case ST_STOP: {
                 break;
             }
         }
--- src/CardReader.cpp
+++ src/CardReader.cpp
@@ -3,19 +3,20 @@
 //
 
 #include "../include/CardReader.h"
+
 //invalid card: 4321543265437654
 CardReader::CardReader() {
 }
 
 bool CardReader::luhnAlgorithm() {
     int nDigits = 16;
-    int parity = nDigits%2;
+    int parity = nDigits % 2;
     int sum = 0;
-    for(int i = 0; i < nDigits; i++){
-        int digit = (int)cardNumber[i]-'0';
-        if(i % 2 == parity)
-            digit = digit*2;
-        if(digit > 9)
+    for (int i = 0; i < nDigits; i++) {
+        int digit = (int) cardNumber[i] - '0';
+        if (i % 2 == parity)
+            digit = digit * 2;
+        if (digit > 9)
             digit = digit - 9;
         sum += digit;
     }
@@ -34,13 +35,9 @@ int CardReader::waitForSwipe() {
     std::string fileName = "/home/kasper/CLionProjects/embeddedProj/swipe";
     std::ifstream inFile(fileName.c_str());
     if (!inFile) {
-        //std::cout << "Unable to open file: " << fileName;
         return 1;
-    }else{
-        //std::cout << "Succesfully opened file: " << fileName << std::endl;
     }
-
-    while ( getline(inFile, line)) {
+    while (getline(inFile, line)) {
         cardNumber = line;
         return 0;
     }
