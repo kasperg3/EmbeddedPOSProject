fe9bd789e6d83222d0bd834236da8387ca948666
nikolaj Ottosen <niklaj_ottosen@hotmail.com>
2019-12-29 10:34:14 +0100

    stringify for receipt

--- src/database/receipt.cpp
+++ src/database/receipt.cpp
@@ -57,3 +57,45 @@ void Receipt::removeReceiptLine(Position pos) {
     amount -= lines.at(pos).total_price;
     lines.erase(lines.begin() + pos);
 }
+
+std::string Receipt::stringifyLine(ReceiptLine line) {
+    std::stringstream stream;
+    Price price = line.price;
+    stream << std::fixed << std::setprecision(2) << price;
+    std::string s = stream.str();
+
+    Name name = line.name;
+    Quantity quantity = line.quantity;
+    Price totalPrice = line.total_price;
+    std::stringstream stream2;
+    stream2 << std::fixed << std::setprecision(2) << totalPrice;
+    std::string s2 = stream2.str();
+
+    std::string out_line = name +" x"+std::to_string(quantity)+" $"+ s+ + "\n itemtotal: " + "$"+s2+"\n";
+   // std::cout << "outline:\n" << out_line << std::endl;
+    return out_line;
+}
+
+std::string Receipt::stringifyReceipt() {
+    std::string output;
+
+    float total = 0.0;
+    std::string total_string;
+    output = "\t  ESD shop\nBest Deals Anywhere\n\n";
+    for(int i = 0; i < lines.size(); i++)
+    {
+        output.append(stringifyLine(lines[i]));
+        total += lines[i].total_price;
+    }
+
+    std::stringstream stream;
+    stream << std::fixed << std::setprecision(2) << total;
+    std::string s = stream.str();
+
+    total_string = "\nTotal: $" + s;
+    output.append(total_string);
+    output.append("\nServiced by: NLO\n");
+
+    std::cout << "test output\n\n" <<  output << std::endl << std::endl;
+    return output;
+}
