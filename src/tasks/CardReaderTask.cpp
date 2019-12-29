0ff05b2343b1985e097cfa07ff1a4eced3c47db3
kasperg3 <kasperg3@rocketmail.com>
2019-12-29 11:13:00 +0100

    Fixed CardReaderTask

--- src/tasks/CardReaderTask.cpp
+++ src/tasks/CardReaderTask.cpp
@@ -5,7 +5,7 @@
 #include "CardReaderTask.hpp"
 
 void *CardReaderTask::taskHandler(CardReaderTask *cardReaderTask) {
-    mq_unlink(QUEUE_BARCODE);
+    mq_unlink(QUEUE_CARDREADER);
 
     /* Create a Queue and wait for it to be created */
     struct mq_attr attr;
@@ -18,38 +18,42 @@ void *CardReaderTask::taskHandler(CardReaderTask *cardReaderTask) {
     do {
         mq = mq_open(QUEUE_CARDREADER, O_CREAT | O_WRONLY, QUEUE_CARDREADER_PERMS, &attr);
         if(mq < 0) {
-            printf("[CardReader]: The queue is not created yet. Waiting...\n");
+            printf("[Cardreader]: The queue is not created yet. Waiting...\n");
             std::this_thread::sleep_for(std::chrono::milliseconds(100));
         }
     } while(mq == -1);
-    printf("[CardReader]: Queue opened, queue descriptor: %d.\n", mq);
+    printf("[Cardreader]: Queue opened, queue descriptor: %d.\n", mq);
     char buffer[QUEUE_CARDREADER_MSGSIZE] = "";
     memset(buffer, 0, sizeof(buffer));   //Removes data on the allocated buffer
-    int charCounter = 0;                   //Initializes the char pointer
+    int charCounter = 0;                    //Initializes the char pointer
+    bool startReading = false;
     while(true) {
         input_event inputEvent = cardReaderTask->readEvent();
-
         if (inputEvent.value == 1) {
-            printf("Value: %s \t Code: 0x%04x (%s) \t Type: %d \n", pressType[inputEvent.value], (int)inputEvent.code, key_names[(int)inputEvent.code], inputEvent.type);
-            if (inputEvent.code != 28) {
-                buffer[charCounter] = *key_names[inputEvent.code];
-                charCounter++;
-            } else {
-                printf("[CardReader] Scanned item with code: %s \n", buffer);
-                if(mq_send(mq, buffer, QUEUE_CARDREADER_MSGSIZE, 0) < 0) {
-                    std::cout <<  strerror(errno) << std::endl;
-                    printf("[CardReader]: CANNOT SEND MESSAGE \n");
+            if (charCounter < QUEUE_CARDREADER_MSGSIZE) {
+                if(startReading){
+                    buffer[charCounter] = *key_names[inputEvent.code];
+                    charCounter++;
                 }
+                if(inputEvent.code == 48){ //The character before the cardnumber
+                    startReading = true;
+                }
+            } else {
+                if(mq_send(mq, buffer, QUEUE_CARDREADER_MSGSIZE, 0) < 0) {
+                    printf("[Cardreader]: CANNOT SEND MESSAGE, %s \n", strerror(errno));
+                }
+                printf("[Cardreader] Read card with code: %s \n", buffer);
                 memset(buffer, 0, sizeof buffer);   //Removes data on the allocated buffer
                 charCounter = 0;
+                startReading = false;
             }
             fflush(stdout);
         }
     }
     /* Cleanup */
-    printf("[BarcocdeScanner]: Cleanup...\n");
+    printf("[Cardreader]: Cleanup...\n");
     mq_close(mq);
-    mq_unlink(QUEUE_BARCODE);
+    mq_unlink(QUEUE_CARDREADER);
 
     return nullptr;
 }
