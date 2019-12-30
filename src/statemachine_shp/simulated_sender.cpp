f2ca1dbc8b63da7ec52711c21bf34e865594e61d
soren <sorenholmpetersen@gmail.com>
2019-12-31 00:50:04 +0100

    Can be used to simulate barcodes being scanned, keyboard being pressed and card being read

---
+++ src/statemachine_shp/simulated_sender.cpp
@@ -0,0 +1,38 @@
+#include "queue.h"
+
+#include <iostream>
+#include <unistd.h>
+#include "statemachine_constants.h"
+using namespace std;
+
+int main()
+{
+    Queue barcode_queue(QUEUE_BARCODE, O_WRONLY | O_NONBLOCK);
+    Queue keyboard_queue(QUEUE_KEYBOARD, O_WRONLY | O_NONBLOCK);
+    Queue card_reader_queue(QUEUE_CARDREADER, O_WRONLY| O_NONBLOCK);
+    Queue customer_display_queue(CUSTOMER_DISPLAY_QUEUE_NAME, O_RDONLY);
+    Queue receipt_queue(QUEUE_RECEIPT, O_RDONLY);
+
+    barcode_queue.flush();
+    keyboard_queue.flush();
+    card_reader_queue.flush();
+
+
+//    barcode_queue.send("5000159461123");
+//    sleep(3);
+//    keyboard_queue.send("ESC");
+//    sleep(3);
+    keyboard_queue.send("1");
+    sleep(3);
+    keyboard_queue.send("5");
+    sleep(3);
+    barcode_queue.send("5701115667007");
+    sleep(3);
+    keyboard_queue.send("<ENTER>");
+
+
+
+    return 0;
+}
+
+
